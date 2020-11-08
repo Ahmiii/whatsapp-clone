09fce6f01cc6b94e355ae7e31583224fdeb465fb
Ahmiii <ahmii.shaikh18@gmail.com>
2020-11-09 00:12:24 +0500

    message bubble

--- src/components/Chat/Chatbody/Chatbody.jsx
+++ src/components/Chat/Chatbody/Chatbody.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Chatbody.css";
 const Chatbody = () => {
-  return <div className="chat__body"></div>;
+  return (
+    <div className="chat__body">
+      <p className={`chat__message ${true && "chat__reciver"}`}>
+        <span style={{ right: "-8px" }}></span>
+        <span className="message__writer">Ahmed Faraz</span>
+        hey
+        ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddguys
+        <span className="message__timestamp">3:20 PM</span>
+      </p>
+    </div>
+  );
 };
 
 export default Chatbody;
