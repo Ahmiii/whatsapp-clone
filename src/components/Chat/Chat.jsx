02243b6c57fb5557c48434b958d5ad8225addd06
Ahmiii <ahmii.shaikh18@gmail.com>
2020-11-08 22:30:14 +0500

    chat body

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Chatheader from "./Chatheader/Chatheader";
+import Chatbody from "./Chatbody/Chatbody";
 import "./Chat.css";
 const Chat = () => {
   return (
     <div className="chat">
       <Chatheader />
+      <Chatbody />
     </div>
   );
 };
---
+++ src/components/Chat/Chatbody/Chatbody.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Chatbody.css";
+const Chatbody = () => {
+  return <div className="chat__body"></div>;
+};
+
+export default Chatbody;
