186de0c81c620bf9dd0775d2aa22bb4cddf6fad7
Ahmiii <ahmii.shaikh18@gmail.com>
2020-11-16 21:03:49 +0500

    firebase authentication

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
 const firebaseConfig = {
   apiKey: "AIzaSyCKYoc7WrFkEt6ekPa2Tx08epb3S9fQ0v0",
   authDomain: "whatsapp-clone-cee3e.firebaseapp.com",
@@ -9,3 +10,11 @@ const firebaseConfig = {
   appId: "1:674054912514:web:032de185e978e226e5451b",
   measurementId: "G-SZ9DWDP60N",
 };
+
+const firebaseapp = firebase.initializeApp(firebaseConfig);
+const whatsappdb = firebaseapp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default whatsappdb;
